65b26f691285da0354157c21c373c625cb23de8f
Hugo Cardoso Silva <hugocardososilva@gmail.com>
2015-06-02 02:11:20 -0300

    aprimorando tela de servico

--- src/main/java/mb/ServicoMB.java
+++ src/main/java/mb/ServicoMB.java
@@ -9,6 +9,7 @@ import javax.faces.bean.ViewScoped;
 import dao.DAO;
 import dao.DAOPrestadorPessoa;
 import model.Pessoa;
+import model.ServicoResidencia;
 
 @ManagedBean
 @ViewScoped
@@ -18,8 +19,17 @@ public class ServicoMB extends AbstractMB {
 		private DAO dao = new DAO();
 		private String senha;
 		private Pessoa prestador;
+		private ServicoResidencia servico;
+		
+		
+		private boolean concluido;
+		private boolean iniciarServico;
+		
+		
 		public ServicoMB() {
 			this.prestadores= new ArrayList<Pessoa>();
+			this.concluido= false;
+			this.iniciarServico= false;
 		}
 
 		public List<Pessoa> getPrestadores() {
@@ -28,8 +38,35 @@ public class ServicoMB extends AbstractMB {
 			prestadores=daop.findAll();
 			dao.close();
 			return prestadores;
+			
+			
 		}
 		
+		
+		
+		public boolean isIniciarServico() {
+			return iniciarServico;
+		}
+
+		public void setIniciarServico(boolean iniciarServico) {
+			this.iniciarServico = iniciarServico;
+		}
+
+		public ServicoResidencia getServico() {
+			return servico;
+		}
+
+		public void setServico(ServicoResidencia servico) {
+			this.servico = servico;
+		}
+
+		public boolean isConcluido() {
+			return concluido;
+		}
+
+		public void setConcluido(boolean concluido) {
+			this.concluido = concluido;
+		}
 
 		public Pessoa getPrestador() {
 			return prestador;
@@ -53,6 +90,14 @@ public class ServicoMB extends AbstractMB {
 		}
 		public String redirectMovimentacaoPrestador(){
 			return "movimentacao-prestador?faces-redirect=true";
+			
+		}
+		public void keyPressSenha(){
+			System.out.println("digitou");
+			this.iniciarServico= true;
+//			displayInfoMessageToUser("senha digitada: " + senha.toString());
+			
+			
 			
 		}
 		
